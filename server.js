1066b57de147dc44436289299e5c4df465dadfb3
mcode36 <48412323+mcode36@users.noreply.github.com>
2020-06-14 19:25:26 -0700

    Update server.js

--- server.js
+++ server.js
@@ -12,10 +12,11 @@ const formidable  = require('formidable');
 const readChunk   = require('read-chunk');
 const fileType    = require('file-type');
 
+var apiRoutes     = require('./routes/api.js');
+
 // App setup
-const PORT = 3000;
 const SECRET_KEY = 'Pjd*j$ljs^jdgQhgdlP0%';
-var sess;
+
 
 
 
@@ -32,135 +33,10 @@ app.use(helmet.dnsPrefetchControl());
 app.use(helmet.referrerPolicy({ policy: 'same-origin' }))
 
 
-//Index page (static HTML)
-app.route('/')
-  .get(function (req, res) {
-    res.sendFile(process.cwd() + '/views/index.html');
-  });
 
-//Posts page (static HTML)
-app.route('/api/posts')
-  .get(function (req, res) {
-    res.sendFile(process.cwd() + '/views/posts.html');
-  });
-app.route('/api/post_01')
-  .get(function (req, res) {
-    res.sendFile(process.cwd() + '/views/view-post.html');
-  });
-app.route('/api/new-post')
-  .get(function (req, res) {
-    res.sendFile(process.cwd() + '/views/new-post.html');
-  });
-app.route('/api/test')
-  .get(function (req, res) {
-    res.sendFile(process.cwd() + '/views/test.html');
-  });
-app.route('/api/account')
-  .get(function (req, res) {
-    res.sendFile(process.cwd() + '/views/account.html');
-  });
 
-// Login
-app.route('/api/login')
-  .get(function (req, res) { 
-    // pass Login form
-    console.log('Route:/api/login ; Method: get');
-    res.sendFile(process.cwd() + '/views/login.html');
-  })
-  .post(function (req, res) {
-    // Process login form data
-    console.log('Route:/api/login ; Method: post');
-    sess = req.session;
-    sess.email = req.body.email;
-    res.sendFile(process.cwd() + '/views/posts.html');
-  })
-// Process login form data
-
-// verify login
-app.route('/api/verify_login')
-  .get(function (req, res) {
-    console.log('Route:/api/verify_login ; Method: get');
-    sess = req.session;
-    if(!sess.email) {
-        return res.redirect('/api/login');
-    }
-    res.sendFile(process.cwd() + '/views/posts.html');
-  });
-
-// Image upload
-app.post('/api/upload', function (req, res) {
-  var photos = [],
-      form = new formidable.IncomingForm();
-
-  // Tells formidable that there will be multiple files sent.
-  form.multiples = true;
-  // Upload directory for the images
-  form.uploadDir = path.join(__dirname, 'tmp_uploads');
-
-  // Invoked when a file has finished uploading.
-  form.on('file', function (name, file) {
-      // Allow only 3 files to be uploaded.
-      if (photos.length === 3) {
-          fs.unlink(file.path);
-          return true;
-      }
-
-      var buffer = null,
-          type = null,
-          filename = '';
-
-      buffer = readChunk.sync(file.path, 0, 262);
-      type = fileType(buffer);
-
-      // Check the file type, must be either png,jpg or jpeg
-      if (type !== null && (type.ext === 'png' || type.ext === 'jpg' || type.ext === 'jpeg')) {
-          // Assign new file name
-          filename = Date.now() + '-' + file.name;
-
-          // Move the file with the new file name
-          fs.rename(file.path, path.join(__dirname, 'uploads/' + filename), (err) => {
-              if (err) throw err;
-          });
-
-          // Add to the list of photos
-          photos.push({
-              status: true,
-              filename: filename,
-              type: type.ext,
-              publicPath: 'uploads/' + filename
-          });
-      } else {
-          photos.push({
-              status: false,
-              filename: file.name,
-              message: 'Invalid file type'
-          });
-          fs.unlink(file.path);
-      }
-  });
-
-  form.on('error', function(err) {
-      console.log('Error occurred during processing - ' + err);
-  });
-
-  // Invoked when all the fields have been processed.
-  form.on('end', function() {
-      console.log('Image upload successful.');
-  });
-
-  // Parse the incoming form fields.
-  form.parse(req, function (err, fields, files) {
-      res.status(200).json(photos);
-  });
-});
-
-//For FCC testing purposes
-// fccTestingRoutes(app);
-
-//Routing for API 
-// apiRoutes(app);
-
-//Sample Front-end
+// Routing for API 
+apiRoutes(app);
 
     
 //404 Not Found Middleware
@@ -171,8 +47,9 @@ app.use(function(req, res, next) {
 });
 
 //Start our server and tests!
-app.listen(PORT, function () {
-  console.log("Listening on port " + PORT);
+
+app.listen(process.env.PORT, function () {
+  console.log("Listening on port " + process.env.PORT);
   if(process.env.NODE_ENV==='test') {
     console.log('Running Tests...');
     setTimeout(function () {
